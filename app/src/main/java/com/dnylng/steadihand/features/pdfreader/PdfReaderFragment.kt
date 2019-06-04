c3a459b8a6d502fc81b1a04f566b38aabcd135e4
Danny Luong <danny.luong@willowtreeapps.com>
2019-06-04 15:21:59 -0400

    Initial gimbal lock

--- app/src/main/java/com/dnylng/steadihand/features/pdfreader/PdfReaderFragment.kt
+++ app/src/main/java/com/dnylng/steadihand/features/pdfreader/PdfReaderFragment.kt
@@ -3,6 +3,10 @@ package com.dnylng.steadihand.features.pdfreader
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.pdf.PdfRenderer
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.ParcelFileDescriptor
 import android.util.Log
@@ -12,7 +16,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.graphics.createBitmap
 import androidx.fragment.app.Fragment
-import com.dnylng.steadihand.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.io.File
 import java.io.FileOutputStream
@@ -43,18 +46,25 @@ class PdfReaderFragment : Fragment() {
     private lateinit var parcelFileDescriptor: ParcelFileDescriptor
     private var pageIdx = 0
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_pdfreader, container, false)
-        view.apply {
-            pdf = findViewById(R.id.pdf)
-            prevPdfBtn = findViewById<FloatingActionButton>(R.id.prev_pdf_btn).also { it.setOnClickListener { showPage(currentPage.index - 1) } }
-            nextPdfBtn = findViewById<FloatingActionButton>(R.id.next_pdf_btn).also { it.setOnClickListener { showPage(currentPage.index + 1) } }
-        }
-        return view
+    private val onLongClickListener = View.OnLongClickListener {
+        isInitReading = true
+        resetPosition(referenceAngles)
+        true
     }
 
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        if (savedInstanceState != null) {
+            pageIdx = savedInstanceState.getInt(SAVED_STATE_PAGE_IDX_KEY)
+        }
+
+        val view = inflater.inflate(com.dnylng.steadihand.R.layout.fragment_pdfreader, container, false)
+        view.apply {
+            pdf = findViewById(com.dnylng.steadihand.R.id.pdf)
+            prevPdfBtn = findViewById<FloatingActionButton>(com.dnylng.steadihand.R.id.prev_pdf_btn).also { it.setOnClickListener { showPage(currentPage.index - 1) } }
+            nextPdfBtn = findViewById<FloatingActionButton>(com.dnylng.steadihand.R.id.next_pdf_btn).also { it.setOnClickListener { showPage(currentPage.index + 1) } }
+        }
+        pdf.setOnLongClickListener(onLongClickListener)
+        return view
     }
 
     override fun onStart() {
@@ -64,6 +74,14 @@ class PdfReaderFragment : Fragment() {
             showPage(pageIdx)
         } catch (e: IOException) {
             Log.d(TAG, e.toString())
+        }
+
+        sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR)?.let {
+            this.rotationSensor = it
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)?.let {
+            this.accelerometer = it
         }
     }
 
@@ -124,6 +142,74 @@ class PdfReaderFragment : Fragment() {
         val pageCount = pdfRenderer.pageCount
         prevPdfBtn.isEnabled = (0 != index)
         nextPdfBtn.isEnabled = (index + 1 < pageCount)
-//        activity?.title = getString(R.string.app_name_with_index, index + 1, pageCount)
+    }
+
+    private lateinit var sensorManager: SensorManager
+    private var rotationSensor: Sensor? = null
+    private var accelerometer: Sensor? = null
+    private var isInitReading = true
+    private val referenceAngles = FloatArray(4)
+
+    private val sensorEventListener = object : SensorEventListener {
+        override fun onAccuracyChanged(rotationSensor: Sensor?, accuracy: Int) { }
+
+        override fun onSensorChanged(event: SensorEvent?) {
+            if (event == null || rotationSensor == null || accelerometer == null) return
+
+            when (event.sensor.type) {
+                Sensor.TYPE_GAME_ROTATION_VECTOR -> {
+                    if (isInitReading) {
+                        calcOrientaionAngles(event.values).copyInto(referenceAngles)
+                        isInitReading = false
+                    } else {
+                        val orientationAngles = calcOrientaionAngles(event.values)
+                        val yaw = orientationAngles[0]
+                        val pitch = orientationAngles[1]
+                        val roll = orientationAngles[2]
+
+                        Log.d(TAG, "ROT VEC -> Yaw: $yaw, Pitch: $pitch, and Roll: $roll")
+
+                        pdf.rotation = -yaw + referenceAngles[0]
+                        pdf.rotationX = -pitch + referenceAngles[1]
+                        pdf.rotationY = -roll + referenceAngles[2]
+                    }
+                }
+                Sensor.TYPE_LINEAR_ACCELERATION -> {
+                    val x = event.values[0]
+                    val y = event.values[1]
+                    val z = event.values[2]
+                    Log.d(TAG, "LIN ACC -> x: $x, y: $y, and z: $z")
+                }
+            }
+        }
+    }
+
+    private fun calcOrientaionAngles(rotationVector: FloatArray): FloatArray {
+        val rotationMatrix = FloatArray(9)
+        val orientationAngles = FloatArray(3)
+
+        SensorManager.getRotationMatrixFromVector(rotationMatrix, rotationVector)
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
+
+        val yawInDegrees = Math.toDegrees(orientationAngles[0].toDouble()).toFloat()
+        val pitchInDegrees = Math.toDegrees(orientationAngles[1].toDouble()).toFloat()
+        val rollInDegrees = Math.toDegrees(orientationAngles[2].toDouble()).toFloat()
+
+        return floatArrayOf(yawInDegrees, pitchInDegrees, rollInDegrees)
+    }
+
+    private fun resetPosition(rotationVector: FloatArray) {
+        calcOrientaionAngles(rotationVector).copyInto(referenceAngles)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(sensorEventListener, rotationSensor, SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_FASTEST)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(sensorEventListener)
     }
 }
